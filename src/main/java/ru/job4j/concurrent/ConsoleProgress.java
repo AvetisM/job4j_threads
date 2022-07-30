3d9a4e4de2e88176fccab5d18c047eda56cfd504
AvetisM <avetis.mkhitaryants@gmail.com>
2022-07-30 14:51:26 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,33 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(50000);
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        char[] process = {'-', '\\', '|', '/'};
+        try {
+            int processIndex = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\r loading... " + process[processIndex++]);
+                if (processIndex == process.length) {
+                    processIndex = 0;
+                }
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
