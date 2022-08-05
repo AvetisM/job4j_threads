c2d536ffd69399e4be24c2dcacb0975890d6c23e
AvetisM <avetis.mkhitaryants@gmail.com>
2022-08-05 08:15:53 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public String getContent(Predicate<Character> filter) throws IOException {
+        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
+            String output = "";
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+            return output;
+        }
+    }
+
+}
---
+++ src/main/java/ru/job4j/io/SaveFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class SaveFile {
+    private final File file;
+
+    public SaveFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (BufferedOutputStream out
+                     = new BufferedOutputStream(new FileOutputStream(file))) {
+            for (int i = 0; i < content.length(); i += 1) {
+                out.write(content.charAt(i));
+            }
+        }
+    }
+}
