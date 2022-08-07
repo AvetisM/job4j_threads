400bf94a46fdf7f4729a201223f8262b7ffd4a78
AvetisM <avetis.mkhitaryants@gmail.com>
2022-08-07 20:21:19 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #329145]

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -3,18 +3,14 @@ package ru.job4j.io;
 import java.io.*;
 import java.util.function.Predicate;
 
-public class ParseFile {
+public final class ParseFile {
     private final File file;
 
     public ParseFile(File file) {
         this.file = file;
     }
 
-    public synchronized File getFile() {
-        return file;
-    }
-
-    public String getContent(Predicate<Character> filter) throws IOException {
+    private synchronized String content(Predicate<Character> filter) throws IOException {
         try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
             StringBuilder output = new StringBuilder();
             int data;
@@ -27,4 +23,11 @@ public class ParseFile {
         }
     }
 
+    public String getContentWithoutUnicode() throws IOException {
+        return content(data -> data < 0x80);
+    }
+
+    public String getContent() throws IOException {
+        return content(data -> true);
+    }
 }
