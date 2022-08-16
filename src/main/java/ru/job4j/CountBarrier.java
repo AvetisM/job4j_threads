884e60dcc8e04765efe9e2703823e52c59050320
AvetisM <avetis.mkhitaryants@gmail.com>
2022-08-16 08:10:52 +0300

    0. Управление нитью через wait. [#6858 #332219]

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -16,19 +16,21 @@ public class CountBarrier {
         this.total = total;
     }
 
-    public synchronized void count() {
-        for (int i = 0; i < total; i++) {
+    public void count() {
+        synchronized (monitor) {
             count++;
             monitor.notifyAll();
         }
     }
 
-    public synchronized void await() {
-        while (count < total) {
-            try {
-                monitor.wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
             }
         }
     }
