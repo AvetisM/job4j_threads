7b19b112fb37d9401b5757b495a9936ecf9c28ed
AvetisM <avetis.mkhitaryants@gmail.com>
2022-08-22 22:00:26 +0300

    3. Денежные переводы AccountStorage [#1104 #332182]

---
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    private final Object monitor = this;
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+
+    public void offer(T value) {
+        synchronized (monitor) {
+            queue.offer(value);
+            notifyAll();
+        }
+    }
+
+    public T poll() {
+        T rls = null;
+        synchronized (monitor) {
+            rls = queue.poll();
+            notifyAll();
+        }
+        return rls;
+    }
+}
